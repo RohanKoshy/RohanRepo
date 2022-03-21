876710e07e8d5d531ce26cbb96026ba21b8b20d0
RohanKoshy <rkosh230@gmail.com>
2022-03-21 07:38:58 -0700

    Week 2

---
+++ src/main/java/Rohanschallenges/TestQueue.java
@@ -0,0 +1,9 @@
+package Rohanschallenges;
+
+public class TestQueue {
+    public static void main(String[] args) {
+        Object[] words = new String[]{"seven", "slimy", "snakes", "sallying", "slowly", "slithered", "southward"};
+        QueueManager qWords = new QueueManager("Words", words);
+
+    }
+}
